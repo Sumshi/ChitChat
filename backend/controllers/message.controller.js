be7d300fc852a594e351a82c7c87ba64696114d8
Sumshi <sumayasomow@gmail.com>
2024-02-23 12:01:37 +0300

    🍻 Correctly implemented messages and conversation between sender and reciever and tested them using pokeman API
    correctly implemented messages and conversation between sender and reciever and tested them using pokeman API

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -3,6 +3,7 @@ import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
 
+// sendMessage controller
 export const sendMessage = async (req, res) => {
     try {
         const { message } = req.body;// get the message from user input
@@ -52,3 +53,28 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 };
+
+// getMessage controller
+export const getMessage = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;// get the id of the user to chat with
+        const senderId = req.user._id;// get the id of the sender from the token
+
+        // find the conversation between the sender and receiver
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");// populate the messages in the conversation
+
+        //if the conversation does not exist, return an empty array
+        if (!conversation) {
+            return res.status(200).json([]);
+        }
+
+        // return the messages in the conversation
+        const messages = conversation.messages;
+        res.status(200).json({ messages });
+    } catch (error) {
+        console.log("error in getMessage controller: ", error.message)
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import { getMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage);
+//eg: http://localhost:5000/api/messages/65d718c43c1f065b37d67c6a
+router.get("/:id", protectRoute, getMessage);//get all messages of a user
+
+// eg: http://localhost:5000/api/messages/send/65d718c43c1f065b37d67c6a
+router.post("/send/:id", protectRoute, sendMessage);// send a message to a user
 
 export default router;
