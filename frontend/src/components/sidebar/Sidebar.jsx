ac612d6272418a69b5aa00be0c6bb216767147a8
Sumshi <sumayasomow@gmail.com>
2024-02-25 17:04:05 +0300

    designed the sidebar with conversations and search

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SearchInput from './SearchInput';
+import Conversations from './Conversations';
 
 const Sidebar = () => {
   return (
     <div>
       <SearchInput />
        <div className="divider px-3"></div>
-      {/* <Conversation />
-      <LogoutButton /> */}
+      <Conversations />
+      {/* <LogoutButton /> */}
     </div>
   )
 }
