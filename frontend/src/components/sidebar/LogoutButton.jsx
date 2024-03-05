9a6de20ddd0e863a950f5506d72a345fc6e20217
Sumshi <sumayasomow@gmail.com>
2024-03-05 11:59:17 +0300

    📌 Done with authentications
    done with authentications

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -2,13 +2,28 @@
 
 import React from 'react'
 import { RiLogoutCircleLine } from "react-icons/ri";
+import useLogout from "../../hooks/useLogout";
+
+// const LogoutButton = () => {
+//   return (
+//     <div className='mt-10'> {/* or mt-auto */}
+//       <RiLogoutCircleLine className='w-6 h-6 text-white cursor-pointer'/>
+//     </div>
+//   )
+// }
 
 const LogoutButton = () => {
-  return (
-    <div className='mt-10'> {/* or mt-auto */}
-      <RiLogoutCircleLine className='w-6 h-6 text-white cursor-pointer'/>
-    </div>
-  )
-}
+	const { loading, logout } = useLogout();
+
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<RiLogoutCircleLine className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
 
 export default LogoutButton;
