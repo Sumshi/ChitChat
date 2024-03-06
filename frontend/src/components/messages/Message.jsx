d1074d36d09b3b555af06a36d0055c99f29a3213
Sumshi <sumayasomow@gmail.com>
2024-03-06 17:11:05 +0300

    🚑️ Messages not getting displayed
    messages not getting displayed

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({message}) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  // const shakeClass = message.shouldShake ? "shake" : "";
+
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
       <div className="avatar online">
         <div className="w-24 rounded-full">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            {/* <img src={conversation.profilePic} alt='user avatar' /> */}
+            <img alt='Tailwind CSS chat bubble component' src={profilePic} />
         </div>
     </div>
 
-      <div className={"chat-bubble text-white bg-blue-500"}>Hi whats up!</div>
+      <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColor}`}>{message.message}</div>
        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12.30</div>
     </div>
   )
